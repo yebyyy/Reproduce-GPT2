5dafed9c3be96fa304f03303ad22599d4982b84e
yebyyy <yexiaomeng2004@gmail.com>
2024-07-02 18:27:28 -0500

    data loader and training in batches

--- train_gpt2.py
+++ train_gpt2.py
@@ -168,6 +168,37 @@ class GPT2(nn.Module):
         return model
 
 # -----------------------------------------------------------------------------------
+# Load Data
+import tiktoken
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        
+        with open("input.txt", "r") as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        # Not moving to GPU here since don't want to waste GPU memory
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current_position = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
+        x = {buf[:-1].view(B, T)}  # inputs
+        y = {buf[1: ].view(B, T)}  # targets
+        # move the position
+        self.current_position += B * T
+        # reset the position if we reach the end
+        if self.current_position + B * T + 1 >= len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+
 # Auto Detect GPU
 device = "cpu"
 if torch.cuda.is_available():
@@ -176,24 +207,15 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print("device: " + device)
 
-# Load Data
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open("input.txt", "r") as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[ : B * T + 1]).to(device)
-x = buf[:-1].view(B, T)
-y = buf[1: ].view(B, t)
-
 # Train
+train_loader = DataLoaderLite(4, 32)
 model = GPT2(GPT2Config())
 model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)  # AdamW optimizer is a bug fix of Adam, it has a weight decay fix, which is a normalization of the gradient
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
